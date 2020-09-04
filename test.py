bd1f9075b3048e0fe384e60266ee856243d72f74
Teodor Sulea <teo.sulea@gmail.com>
2020-09-04 11:17:48 +0300

    added test script

---
+++ test.py
@@ -0,0 +1,31 @@
+import TASKit
+import numpy as np
+
+import sys
+
+##Test load
+test_load = TASKit.TrajHandle.TrajLoader(prmtop="test/Peptide.prmtop", DCDS="test/Peptide.dcd", stride=1)
+print ("Test Load: {}".format(test_load))
+
+##Test EP timeseries generation
+
+
+EPTSCorr = test_load.GetPotentialEnergies(ProgressInt=10000)
+print ("EPTS Corr: {}".format(EPTSCorr))
+
+##Decorrelate EP TS
+EPTSDecorr = TASKit.Decorrelation.DecorrelateTimeseries(EPTSCorr, stride=100)
+print ("EPTS Decorr: {}".format(EPTSDecorr))
+
+##Generate Plot
+
+EPTSDecorr.GenerateNEFFPlot()
+
+##Print decorr frames
+DecorrFrames = EPTSDecorr.GetDecorrSamples()
+print ("Decorr Frames: {}".format(DecorrFrames))
+
+##Generate new DCD with Decorr Frames
+TrimmedTraj = test_load.TrimTraj(DecorrFrames)
+print ("TrimmedTraj: {}".format(TrimmedTraj))
+
