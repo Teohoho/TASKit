613b8b832cda0e03e4b69c9da92265388683c1d8
Teohoho <teo.sulea@gmail.com>
2020-09-09 10:50:31 +0300

    Added Licence and Licence Notices on source files

--- TASKit/Cluster.py
+++ TASKit/Cluster.py
@@ -1,3 +1,20 @@
+##Licence notices
+#    TASKit: A Python-based kit containing Post-MD analysis tools.
+#    Copyright (C) 2020  Șulea Teodor Asvadur
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>
+
 #imports
 import scipy.cluster.hierarchy as sch
 from scipy.spatial.distance import squareform
